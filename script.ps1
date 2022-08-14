fa2909ee74ab309a764a5612edb66ce296f1a099
alvinirving <alvinirving1@yahoo.com>
2022-08-14 20:13:22 +0100

    Added initial project plan

--- script.ps1
+++ script.ps1
@@ -3,3 +3,4 @@ Temporary commit content for 2022-07-04T20:13:22
 Temporary commit content for 2022-07-17T20:13:22
 Temporary commit content for 2022-08-01T20:13:22
 Temporary commit content for 2022-08-04T20:13:22
+Temporary commit content for 2022-08-14T20:13:22

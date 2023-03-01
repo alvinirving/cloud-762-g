fbfa081a5e7dc04bb42fcd5d91db807ab0e8aa1d
alvinirving <alvinirving1@yahoo.com>
2023-03-01 20:13:22 +0100

    Refactored code for better readability

--- analysis.py
+++ analysis.py
@@ -20,3 +20,4 @@ Temporary commit content for 2023-01-21T20:13:22
 Temporary commit content for 2023-01-29T20:13:22
 Temporary commit content for 2023-02-17T20:13:22
 Temporary commit content for 2023-02-28T20:13:22
+Temporary commit content for 2023-03-01T20:13:22

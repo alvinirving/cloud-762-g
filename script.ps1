ce7ba301b6adb302a3d2bd83cedc302f6c36408f
alvinirving <alvinirving1@yahoo.com>
2024-03-16 20:13:22 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -49,3 +49,4 @@ Temporary commit content for 2024-02-01T20:13:22
 Temporary commit content for 2024-02-09T20:13:22
 Temporary commit content for 2024-02-25T20:13:22
 Temporary commit content for 2024-03-15T20:13:22
+Temporary commit content for 2024-03-16T20:13:22

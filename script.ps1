98a838e9b8cf4d75ea1f97969bbb03f9327339de
alvinirving <alvinirving1@yahoo.com>
2024-12-16 20:13:22 +0100

    Improved error handling logic

--- script.ps1
+++ script.ps1
@@ -74,3 +74,4 @@ Temporary commit content for 2024-10-07T20:13:22
 Temporary commit content for 2024-10-24T20:13:22
 Temporary commit content for 2024-11-09T20:13:22
 Temporary commit content for 2024-12-13T20:13:22
+Temporary commit content for 2024-12-16T20:13:22
